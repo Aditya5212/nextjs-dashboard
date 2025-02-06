656aa34a3b85bccad3c4fc6c93ff7c3d892eb99b
Aditya <adityadange135@gmail.com>
2025-02-06 13:13:12 +0530

    Navigating Between Pages

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import Layout from "@/app/dashboard/layout"
+
+
+function page() {
+  return (
+    <>
+        <div>
+            This is Dashboard !    
+        </div>
+    </>
+    
+  )
+}
+
+export default page
